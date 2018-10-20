fa9e0ee669a437ee77e78238b7231061cd55e300
Nicolas Bærbar <Nicolas.dyhrman@gmail.com>
2018-10-20 23:27:37 +0200

    Core changes

--- js/Core.js
+++ js/Core.js
@@ -22,37 +22,4 @@ class CoreCrystal extends GameObject
         this.Tiles.push(Tile);
         //console.log(this.Tiles);
     }
-
-    onCollide(gameObject)
-    {
-        let tag_IsEnemy = false;
-        console.log("Collided");
-        for (let i = 0; i < gameObject.tags.length; i++)
-        {
-            let tag = gameObject.tags[i];
-
-            if(tag == 'Enemey')
-            {
-                tag_IsEnemy = true;
-            }
-            
-        }
-
-        if(tag_IsEnemy)
-        {
-            let damage = 0;
-
-            if(typeof gameObject.damage == 'undefined')
-            {
-                damage = 1;
-            }
-            else
-            {
-                damage = gameObject.damage;
-            }
-
-            this.game.health -= damage;
-            gameObject.destroy();
-        }
-    }
 }
